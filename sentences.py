3d60e1a306190104d4221c7871c06621c1edba70
dutsonc <dut20001@byui.edu>
2024-06-21 20:35:35 -0600

    got working with preposition phrase

--- sentences.py
+++ sentences.py
@@ -19,7 +19,7 @@ def make_sentence(quantity, tense):
     """
     quantity=quantity
     tense=tense
-    sentence = str.capitalize(f"{get_determiner(quantity)} {get_noun(quantity)} {get_verb(quantity, tense)} {get_preposition()} {get_prepositional_phrase(quantity)}.")
+    sentence = str.capitalize(f"{get_determiner(quantity)} {get_noun(quantity)} {get_verb(quantity, tense)} {get_prepositional_phrase(quantity)}.")
     capitalized_sentence=str.capitalize(sentence)
     return sentence
 def get_determiner(quantity):
@@ -118,25 +118,35 @@ def generate_quantity():
 def get_preposition():
     """Return a randomly chosen preposition
     from this list of prepositions:
-    "about", "above", "across", "after", "along",
-    "around", "at", "before", "behind", "below",
-    "beyond", "by", "despite", "except", "for",
-    "from", "in", "into", "near", "of",
-    "off", "on", "onto", "out", "over",
-    "past", "to", "under", "with", "without"
+        "about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"
     Return: a randomly chosen preposition.
     """
+    prepositions = ["about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"]
+    preposition=random.choice(prepositions)
+    return preposition
 def get_prepositional_phrase(quantity):
     """Build and return a prepositional phrase composed
     of three words: a preposition, a determiner, and a
     noun by calling the get_preposition, get_determiner,
     and get_noun functions.
     Parameter
-    quantity: an integer that determines if the
-    determiner and noun in the prepositional
-    phrase returned from this function should
-    be single or pluaral.
+        quantity: an integer that determines if the
+            determiner and noun in the prepositional
+            phrase returned from this function should
+            be single or pluaral.
     Return: a prepositional phrase.
     """
-
+    
+    prepositionalPhrase= (f"{get_preposition()} {get_determiner(quantity)} {get_noun(quantity)}")
+    return prepositionalPhrase
 main()
